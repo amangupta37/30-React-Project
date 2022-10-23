9b83d254f118a130ea8fe4bbececd13f6169c81a
Aman Gupta <amangupta6121998@gmail.com>
2022-10-23 07:58:16 +0530

    refactor: add updated folder structure

---
+++ 01-calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./layouts/Calculator";
+import "./App.scss";
+const App = () => {
+    return (
+        <div className="App">
+            <Calculator />
+        </div>
+    );
+};
+
+export default App;
