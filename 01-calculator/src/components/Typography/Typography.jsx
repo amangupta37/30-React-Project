30991fc9e5d2a256af7bf206c55ab827081f7a92
Aman Gupta <amangupta6121998@gmail.com>
2022-10-25 22:31:38 +0530

    refactor: add imported utility function for variant styling

--- 01-calculator/src/components/Typography/Typography.jsx
+++ 01-calculator/src/components/Typography/Typography.jsx
@@ -1,39 +1,16 @@
 import React from "react";
-
+import { CustomizeVariant } from "../../utils";
 const Typography = (props) => {
     const { variant, align, size, color, children, className, style } = props;
 
     const Component = variant ? variant : "p";
 
-    const CustomizeVariant = () => {
-        const sx = style ? style : "";
-
-        const CustomStyle = {
-            textAlign: align ? align : "",
-            fontSize: size ? size : "",
-            color: color ? color : "",
-
-            /* Add custom margin */
-            margin: props.m ? props.m : "",
-            marginTop: props.mt ? props.mt : "",
-            marginLeft: props.ml ? props.ml : "",
-            marginRight: props.mr ? props.mr : "",
-
-            /* Add custom padding */
-            padding: props.p ? props.p : "",
-            paddingTop: props.pt ? props.pt : "",
-            paddingLeft: props.pl ? props.pl : "",
-            paddingRight: props.pr ? props.pr : "",
-
-            /* Add additional styling */
-            ...sx,
-        };
-
-        return CustomStyle;
-    };
-
     return (
-        <Component className={className} style={CustomizeVariant()}>
+        <Component
+            className={className}
+            style={CustomizeVariant(align, size, color, style)}
+            {...props}
+        >
             {children}
         </Component>
     );
