24d65b04fda83f9d29e64d7404213b18fd53c40d
Aman Gupta <amangupta6121998@gmail.com>
2023-01-20 23:08:51 +0530

    refactor(component/index): add dynamic import for components

---
+++ 01-calculator/src/components/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button/Button";
+import Input from "./Input/Input";
+import Toggle from "./Toggle/Toggle";
+import Typography from "./Typography/Typography";
+
+export { Button, Input, Toggle, Typography };
